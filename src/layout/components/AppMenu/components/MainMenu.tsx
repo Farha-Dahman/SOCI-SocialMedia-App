1cb539716cf8fb210a42e6e978d46ada991a10d7
Farha Dahman <farhajehad@gmail.com>
2023-08-22 10:36:45 +0300

    modifying the PostDetails module

--- src/layout/components/AppMenu/components/MainMenu.tsx
+++ src/layout/components/AppMenu/components/MainMenu.tsx
@@ -10,7 +10,11 @@ const MainMenu: React.FC = () => {
   return (
     <nav className="menu mt-4 mb-5 shortcuts ps-4">
       {menuItems.map((item, index) => (
-        <a href="#" className="mb-3 d-flex align-items-center" key={index}>
+        <a
+          href={item.text === "Feed" ? "/" : "#"}
+          className="mb-3 d-flex align-items-center"
+          key={index}
+        >
           <FontAwesomeIcon icon={item.icon} className="icon me-3" />
           <div>{item.text}</div>
         </a>
