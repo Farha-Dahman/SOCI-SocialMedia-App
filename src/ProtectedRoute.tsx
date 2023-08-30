44ceabf528a9aaedea220f59ebacf43d512f1ed2
Farha Dahman <farhajehad@gmail.com>
2023-08-30 15:38:32 +0300

    modifying the protected route component

--- src/ProtectedRoute.tsx
+++ src/ProtectedRoute.tsx
@@ -1,11 +1,13 @@
 import React, { ReactNode } from "react";
 import { Navigate } from "react-router-dom";
+import { useAppStore } from "./hooks/useAppStore";
 
 interface ProtectedRouteProps {
   children: ReactNode;
 }
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
-  if (!localStorage.getItem("user_name")) {
+  const { username } = useAppStore();
+  if (!username) {
     return <Navigate to="/login" />;
   }
   return <>{children}</>;
