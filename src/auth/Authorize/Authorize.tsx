4548f4fd596a9ce411cb8340297cb336a3000cfc
Farha Dahman <farhajehad@gmail.com>
2023-09-03 14:05:47 +0300

    modifying the Authorize component

--- src/auth/Authorize/Authorize.tsx
+++ src/auth/Authorize/Authorize.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from "react";
 import { useAppStore } from "../../hooks/useAppStore";
+import { PageNotFound } from "../../layout/components/PageNotFound/PageNotFound";
 
 interface AuthorizeProps {
   allowedRoles: string[];
@@ -9,7 +10,7 @@ interface AuthorizeProps {
 const Authorize: React.FC<AuthorizeProps> = ({ allowedRoles, children }) => {
   const { role } = useAppStore();
   const isAllowed = allowedRoles.includes(role);
-  return isAllowed ? <>{children}</> : null;
+  return isAllowed ? <>{children}</> : <PageNotFound />;
 };
 
 export default Authorize;
